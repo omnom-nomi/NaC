dfdb342eb73a8544e9c209c09bdb72ba9d5d693b
omnom-momi <morisnaomi13@gmail>
2023-10-25 21:32:21 +0100

    #

--- finalGame.java
+++ finalGame.java
@@ -18,29 +18,36 @@ public class finalGame {
             if (gameOver(gameBoard)) {
                 break;
             }
-
         }
-
     }
 
     public static boolean gameOver(Board board) {
         char winner = board.checkWinner();
+
         if (winner == 'x') {
             System.out.println("You win.");
+            return true;
         } else if (winner == 'o') {
             System.out.println("The computer wins.");
             return true;
         }
 
+        if (isDraw(board)) {
+            System.out.println("It's a draw.");
+            return true;
+        }
+
+        return false; // Return false if no winner and it's not a draw
+    }
+
+    public static boolean isDraw(Board board) {
         for (int i = 0; i < Board.SIZE; i++) {
             for (int j = 0; j < Board.SIZE; j++) {
                 if (board.getGameBoard()[i][j] == '-') {
-                    return false;
+                    return false; // Return false immediately if an empty spot is found.
                 }
             }
         }
-
-        System.out.println("It's a draw");
-        return true;
+        return true; // Return true if no empty spots found (indicating a draw).
     }
 }
