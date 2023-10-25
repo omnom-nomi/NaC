921cc42afbcc6753e9cba0ba6bdb54201c41d00e
omnom-momi <morisnaomi13@gmail>
2023-10-25 11:32:10 +0100

    #

--- Player.java
+++ Player.java
@@ -8,24 +8,24 @@ public class Player {
         this.gameBoard = board;
     }
 
-    public boolean makeMove(int x, int y) {
-        // Make the player's move and return success status
-        // check if it's the player's turn currently
-        boolean player = true;
+    public boolean makeMove() {
+        while (true) {
 
-        System.out.println("Please enter the row");
-        int a = sc.nextInt();
-        System.out.println("Please enter the column");
-        int b = sc.nextInt();
+            System.out.println("Please enter the row");
+            int a = sc.nextInt();
+            System.out.println("Please enter the column");
+            int b = sc.nextInt();
 
-        if (a < 0 || b < 0 || a >= Board.SIZE || b >= Board.SIZE) {
-            System.out.println("Out of bounds;");
-        } else if (gameBoard.getGameBoard()[a][b] != '-') {
-            System.out.println("Someone already took that space");
+            if (a < 0 || b < 0 || a >= Board.SIZE || b >= Board.SIZE) {
+                System.out.println("Out of bounds;");
+                continue;
+            } else if (gameBoard.getGameBoard()[a][b] != '-') {
+                System.out.println("Someone already took that space");
+                continue;
+            } else {
+                gameBoard.placeMove(a, b, 'x');
+                return true;
+            }
         }
-        ;
-
-        return false;
-
     }
 }
