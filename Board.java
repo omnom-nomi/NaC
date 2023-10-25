ea20dbe3d886563ddbbecf389cd314e5d2150d3e
omnom-momi <morisnaomi13@gmail>
2023-10-25 11:40:35 +0100

    checked if the player's move is valid

--- Board.java
+++ Board.java
@@ -24,8 +24,13 @@ public class Board {
 
     }
 
-    public boolean placeMove(int x, int y, char player) {
-        return false;
+    public boolean placeMove(int row, int column, char b) {
+        if (row >= 0 && row < SIZE && column >= 0 && column < SIZE && gameBoard[row][column] == '-') {
+            gameBoard[row][column] = b;
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
