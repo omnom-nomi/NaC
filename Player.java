c9e6ab6e94368d57d032a959d68e1a98e6bba8bb
omnom-momi <morisnaomi13@gmail>
2023-10-31 18:34:55 +0000

    added an exception code to handle errors by player

--- Player.java
+++ Player.java
@@ -9,23 +9,31 @@ public class Player {
     }
 
     public boolean makeMove() {
+
         while (true) {
 
-            System.out.println("Please enter the column (0,1, or 2): ");
-            int row = sc.nextInt();
-            System.out.println("Please enter the row (0,1, or 2): ");
-            int column = sc.nextInt();
+            try {
 
-            if (row < 0 || column < 0 || row >= Board.SIZE || column >= Board.SIZE) {
-                System.out.println("Out of bounds.");
-                continue;
-            } else if (gameBoard.getGameBoard()[row][column] != '-') {
-                System.out.println("That space has already been taken.");
-                continue;
-            } else {
-                gameBoard.placeMove(row, column, 'x');
-                return true;
+                System.out.println("Please enter the column (0,1, or 2): ");
+                int row = sc.nextInt();
+                System.out.println("Please enter the row (0,1, or 2): ");
+                int column = sc.nextInt();
+
+                if (row < 0 || column < 0 || row >= Board.SIZE || column >= Board.SIZE) {
+                    System.out.println("Out of bounds.");
+                    continue;
+                } else if (gameBoard.getGameBoard()[row][column] != '-') {
+                    System.out.println("That space has already been taken.");
+                    continue;
+                } else {
+                    gameBoard.placeMove(row, column, 'x');
+                    return true;
+                }
+            } catch (Exception e) {
+                System.err.println("Invalid input. Try again");
+                sc.nextLine();
             }
         }
     }
+
 }
